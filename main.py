a2b6987ad4f931d9ad06270349103e96f52f90ce
Ozgur Ersoz <ozgur.ersoz3@gmail.com>
2025-05-05 20:49:26 +0300

    moving around

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ def main():
     dt = 0
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-   
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
+
         screen.fill("black")
         player.draw(screen)
-        pygame.display.flip()  
+        pygame.display.flip()
 
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
